8d7ba3dd7a6900155224ae55e151db1fba749c39
Don Schniitzius <don@sketchbookclub.com>
2022-08-04 01:00:45 -0500

    Color Set Generator

---
+++ _color set generator/BMC_Generator.py
@@ -0,0 +1,89 @@
+# BMC GENERATOR
+
+# WRITE THIS SCRIPT TO FILE WITH:
+# python BMC_Generator.py > moods.py
+
+
+from PIL import Image, ImageDraw, ImageColor
+import os
+
+
+# IMPORT VARS
+from mood_vars import *
+
+
+dir = './icons/'
+if not os.path.isdir(dir):
+    os.mkdir(dir)
+
+
+# WRITE HEADER
+print("#",panel_name,"COLORS SET")
+print("\nimport bpy")
+print("\nfrom .globals import *")
+print("from .color_functions import *")
+
+
+# WRITE OPERATORS
+print("\n#",panel_name,"OPERATORS")
+for i in array:
+  print("\nclass",i[0]+"(bpy.types.Operator):")
+  print("    \"\"\""+i[1]+"\"\"\"")
+  print("    bl_label = \""+i[2]+"\"")
+  print("    bl_idname = 'color."+i[3]+"'")
+  print("    def execute(self, context):")
+  print("        set_base_color(0x"+i[4]+", self.bl_label)")
+  print("        return {'FINISHED'}")
+
+
+# WRITE PANEL
+print("\n")
+print("#",panel_name,"PANEL")
+print("")
+print("class "+panel_label+"Panel(bpy.types.Panel):")
+print("    bl_idname = \""+panel_name+"_PT_Panel\"")
+print("    bl_label = \""+panel_label+"\"")
+print("    bl_space_type = \"VIEW_3D\"")
+print("    bl_region_type = \"UI\"")
+print("    bl_category = \"MAT\"")
+print("    bl_parent_id = 'QMC_PT_Panel'")
+print("    bl_options = {'DEFAULT_CLOSED'}")
+print("")
+print("    def draw(self, context):")
+print("        global c_icons")
+print("        layout = self.layout")
+print("")
+print("        srow = layout.row()")
+print("        scol = srow.column(align=True)")
+print("        scol.scale_y = 1.25")
+for i in array:
+  print("        scol.label(text=\"\", icon_value=g.c_icons[\""+i[3]+"\"].icon_id)")
+print("")
+print("        scol = srow.column(align=True)")
+print("        scol.scale_y = 1.25")
+print("        scol.scale_x = 3.0")
+for i in array:
+  print("        scol.operator(\"color."+i[3]+"\", text=\""+i[2]+"\")")
+
+
+# WRITE CLASSES
+print("\n")
+print("#",panel_name,"CLASSES")
+print(array_name+" = [")
+print("    "+panel_label+"Panel,")
+for i in array:
+  print("    "+i[0]+",")
+print("]")
+
+
+# CREATE PNG ICONS
+# !!! REMEMBER TO RUN ImageOptim ON FOLDER !!!
+for i in array:
+  # name of the file to save
+  filename = i[3]+".png"
+  file_path = os.path.join(dir, filename)
+  # create new image
+  col = ImageColor.getcolor("#"+i[4], "RGB")
+  image = Image.new(mode = "RGB", size = (32,32), color = col)
+  # save the file
+  image.save(file_path)
---
+++ _color set generator/mood_vars.py
@@ -0,0 +1,33 @@
+
+set_name    = "Moods"
+panel_label = set_name
+panel_name  = set_name.upper()
+array_name  = "array_"+set_name.lower()
+
+array = [
+# ["Class Name","Tool Tip","Button Label","ID Name","Hex Color"],
+["MoodsGreen02","Soft sage green with a hint of blue","Green 02","moods_green_02","8A9082"],
+["MoodsBlue02","Fresh clean minty blue","Blue 02","moods_blue_02","d3dbd9"],
+["MoodsBlue08","Vibrant mid-tone sky blue","Blue 08","moods_blue_08","abccd3"],
+["MoodsPink04","Muted pink with a touch of grey","Pink 04","moods_pink_04","F3E0E4"],
+["MoodsGreen07","Foliage green with hint of blue & yellow","Green 07","moods_green_07","698a66"],
+["MoodsGreen08","Fresh neo green","Green 08","moods_green_08","a2c3af"],
+["MoodsGreen13","Sage green with added optimism","Green 13","moods_green_13","b9ccae"],
+["MoodsYellow06","Daffodil yellow with a lemon tang","Yellow 06","moods_yellow_06","ffe887"],
+["MoodsPink07","A romantic, muted pink","Pink 07","moods_pink_07","d2c5bc"],
+["MoodsPink08","Dusty pink on a distinctly earthy base","Pink 08","moods_pink_08","c2a79c"],
+["MoodsBeige02","Light buff brown with touch of grey","Beige 02","moods_beige_02","b6a18b"],
+["MoodsBeige03","Soft, earthy beige with notes of grey and yellow","Beige 03","moods_beige_03","dfdbcf"],
+["MoodsBlue06","Deep slate blue","Blue 06","moods_blue_06","4c5f6b"],
+["MoodsBlue07","Dark inky blue with touch of green","Blue 07","moods_blue_07","3d4f56"],
+["MoodsBlue17","A grounded, muted blue","Blue 17","moods_blue_17","8398a5"],
+["MoodsBlue111","Cobalt keyworker blue","Blue1 11","moods_blue1_11","1a4677"],
+["MoodsPurple03","Deep, velvety purple with blue undertones","Purple 03","moods_purple_03","432d2e"],
+["MoodsTeal03","Charcoal blue with grey undertones","Teal 03","moods_teal_03","596869"],
+["MoodsRed03","Deep terracotta","Red 03","moods_red_03","c3867b"],
+["MoodsBlack01","Graphite black with blue undertones","Black 01","moods_black_01","444A4C"],
+["MoodsWhite04","Soft linen white with grey undertones","White 04","moods_white_04","e4e2d7"],
+["MoodsWhite03","Soft creamy white with touch of yellow","White 03","moods_white_03","f3f1e5"],
+["MoodsWhite02","Off-white with a touch of grey","White 02","moods_white_02","eceae2"],
+["MoodsWhite01","Pure white with the lightest touch of grey","White 01","moods_white_01","f2f2eb"],
+]
