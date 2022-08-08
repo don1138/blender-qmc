58c81411155392e2da6c57c2374156d294d01193
Don Schniitzius <don@sketchbookclub.com>
2022-08-08 14:52:31 -0500

    Color Set Generator

---
+++ _color set generator/Array_Generator.py
@@ -0,0 +1,24 @@
+# ARRAY GENERATOR
+
+# WRITE THIS SCRIPT TO FILE WITH:
+# python Array_Generator.py > ral.py
+
+set_name = "RAL"
+
+pre_array = [
+["1000","Green beige","CDBA88"],
+["1001","Beige","D0B084"],
+["1002","Sand yellow","D2AA6D"],
+]
+
+print("")
+print("set_name    = \""+set_name+"\"")
+print("panel_label = set_name")
+print("panel_name  = set_name.upper()")
+print("array_name  = \"array_\"+set_name.lower()")
+print("\narray = [")
+
+for i in pre_array:
+  print("[\""+set_name+i[0]+"\",\""+set_name+i[0]+" - "+i[1]+"\",\""+set_name,i[0]+"\",\""+set_name.lower()+"_"+i[0]+"\",\""+i[2]+"\"],")
+
+print("]")
